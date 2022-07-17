9477de9ff0f0ed4f8b713d9da3a411b13c8e309b
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2022-07-17 18:08:02 +0900

    Fix Hydration Error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/globals";
 import { media } from "../styles/theme";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient({
@@ -11,8 +14,23 @@ function MyApp({ Component, pageProps }) {
       },
     },
   });
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === "undefined") {
+    return <></>;
+  }
+
   return (
     <>
+      <Head>
+        <title>My blog app</title>
+      </Head>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={media}>
           <GlobalStyle />
