168fcd61e742224a7e1f5a74a65056ee9195ba6c
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-02-12 16:49:15 +0900

    Add auth api
    - 회원 인증 api 생성(회원가입, 로그인)

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   hashedPassword: String,
 });
 
+// 인스턴스 메서드 - this는 문서 인스턴스(UserSchema)를 가리킴
 UserSchema.methods.setPassword = async function(password) {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
@@ -16,6 +17,13 @@ UserSchema.methods.checkPassword = async function(password) {
   return result;
 };
 
+UserSchema.methods.serialize = function(){
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+}
+
+// 스태틱 메서드 - this는 모델(User)을 가리킴
 UserSchema.statics.findByUsername = function(username) {
   return this.findOne({ username });
 };
