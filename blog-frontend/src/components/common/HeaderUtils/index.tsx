84990f4e5e5bec59f50b485247b40153fa567285
seunghyecho <tmdgp523@naver.com>
2025-01-17 14:13:28 +0900

    빌드 오류 수정

--- blog-frontend/src/components/common/HeaderUtils/index.tsx
+++ blog-frontend/src/components/common/HeaderUtils/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { BiEditAlt, BiSearchAlt, BiUser } from 'react-icons/Bi';
+import { MdAdd } from 'react-icons/md';
 import Modal from 'components/common/Modal';
 import palette from 'lib/styles/palette';
 
@@ -22,52 +22,52 @@ function HeaderUtils({ user }) {
   const router = useRouter();
   const [modal, setModal] = useState(false);
 
-  const onCreate = ()=>{
+  const onCreate = () => {
     setModal(true);
-  }
+  };
 
-  const onCancel = ()=>{
+  const onCancel = () => {
     setModal(false);
-  }
-  
-  const onConfirm = ()=>{
+  };
+
+  const onConfirm = () => {
     setModal(false);
     router.push('/create');
     window.localStorage.removeItem('_id');
-  }
+  };
 
   return (
-   <>
+    <>
       <Utils>
         {user && (
-            <li>
-              <button onClick={onCreate}>
-                <BiEditAlt size={24} color={palette.cyan[5]}/>
-              </button>
-            </li>
-          )}
+          <li>
+            <button onClick={onCreate}>
+              <MdAdd size={24} color={palette.cyan[5]} />
+            </button>
+          </li>
+        )}
 
         <li>
-          <Link href='/search'>
-              <BiSearchAlt size={24} />
+          <Link href="/search">
+            <MdAdd size={24} />
           </Link>
         </li>
         <li>
           <Link href={user ? '/accounts' : '/login'}>
-              <BiUser size={24} />
+            <MdAdd size={24} />
           </Link>
         </li>
       </Utils>
 
-      <Modal 
-          visible={modal} 
-          title='포스트 등록' 
-          description='포스트를 등록하시겠습니까?'
-          confirmText='등록'
-          onConfirm={onConfirm} 
-          onCancel={onCancel} 
+      <Modal
+        visible={modal}
+        title="포스트 등록"
+        description="포스트를 등록하시겠습니까?"
+        confirmText="등록"
+        onConfirm={onConfirm}
+        onCancel={onCancel}
       />
-   </>
+    </>
   );
 }
 
