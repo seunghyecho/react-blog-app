5b1dd12cbb56f9c197a1507985610dcf4aed64a2
seunghyecho <tmdgp523@naver.com>
2025-02-16 21:04:48 +0900

    모바일 화면 비율 적용
    - TODO

--- blog-frontend/src/pages/_document.tsx
+++ blog-frontend/src/pages/_document.tsx
@@ -10,6 +10,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
           <link
             rel="stylesheet"
             type="text/css"
